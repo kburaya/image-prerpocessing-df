a2c32621f1440b8fa88cffc91b7987fb57183eca
k.buraya <k.buraya@kburaya-nb.local>
2019-03-23 23:34:29 +0300

    face detection, text detection

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,20 +1,137 @@
+# import the necessary packages
 import argparse
+import time
 from os import listdir
 from os.path import isfile, join
 
 import colorgram
+import cv2
+import imutils
 import numpy as np
 from keras.applications.resnet50 import ResNet50
 from keras.applications.resnet50 import preprocess_input
 from keras.preprocessing import image
+from nms import nms
+from opencv_text_detection import utils
+from opencv_text_detection.decode import decode
 from scipy import ndimage as ndi
-from skimage import measure
 from skimage import color
+from skimage import measure
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--folder", required=False, help="path to the input folder")
 args = vars(ap.parse_args())
+
+
+def image_colorfulness(image):
+    # split the image into its respective RGB components
+    (B, G, R) = cv2.split(image.astype("float"))
+
+    # compute rg = R - G
+    rg = np.absolute(R - G)
+
+    # compute yb = 0.5 * (R + G) - B
+    yb = np.absolute(0.5 * (R + G) - B)
+
+    # compute the mean and standard deviation of both `rg` and `yb`
+    (rbMean, rbStd) = (np.mean(rg), np.std(rg))
+    (ybMean, ybStd) = (np.mean(yb), np.std(yb))
+
+    # combine the mean and standard deviations
+    stdRoot = np.sqrt((rbStd ** 2) + (ybStd ** 2))
+    meanRoot = np.sqrt((rbMean ** 2) + (ybMean ** 2))
+
+    # derive the "colorfulness" metric and return it
+    return stdRoot + (0.3 * meanRoot)
+
+
+def text_detect(image, east, min_confidence, width, height):
+    # load the input image and grab the image dimensions
+    image = cv2.imread(image)
+    orig = image.copy()
+    (origHeight, origWidth) = image.shape[:2]
+
+    # set the new width and height and then determine the ratio in change
+    # for both the width and height
+    (newW, newH) = (width, height)
+    ratioWidth = origWidth / float(newW)
+    ratioHeight = origHeight / float(newH)
+
+    # resize the image and grab the new image dimensions
+    image = cv2.resize(image, (newW, newH))
+    (imageHeight, imageWidth) = image.shape[:2]
+
+    # define the two output layer names for the EAST detector model that
+    # we are interested -- the first is the output probabilities and the
+    # second can be used to derive the bounding box coordinates of text
+    layerNames = [
+        "feature_fusion/Conv_7/Sigmoid",
+        "feature_fusion/concat_3"]
+
+    # load the pre-trained EAST text detector
+    #     print("[INFO] loading EAST text detector...")
+    net = cv2.dnn.readNet(east)
+
+    # construct a blob from the image and then perform a forward pass of
+    # the model to obtain the two output layer sets
+    blob = cv2.dnn.blobFromImage(image, 1.0, (imageWidth, imageHeight), (123.68, 116.78, 103.94), swapRB=True,
+                                 crop=False)
+
+    start = time.time()
+    net.setInput(blob)
+    (scores, geometry) = net.forward(layerNames)
+    end = time.time()
+
+    # NMS on the the unrotated rects
+    confidenceThreshold = min_confidence
+    nmsThreshold = 0.4
+
+    # decode the blob info
+    (rects, confidences, baggage) = decode(scores, geometry, confidenceThreshold)
+
+    offsets = []
+    thetas = []
+    for b in baggage:
+        offsets.append(b['offset'])
+        thetas.append(b['angle'])
+
+    ##########################################################
+
+    functions = [nms.felzenszwalb.nms, nms.fast.nms, nms.malisiewicz.nms]
+
+    drawpolys_count, drawrects_count = 0, 0
+
+    for i, function in enumerate(functions):
+
+        start = time.time()
+        indicies = nms.boxes(rects, confidences, nms_function=function, confidence_threshold=confidenceThreshold,
+                             nsm_threshold=nmsThreshold)
+        end = time.time()
+
+        indicies = np.array(indicies).reshape(-1)
+
+        if len(indicies) != 0:
+            drawrects = np.array(rects)[indicies]
+            drawpolys_count = len(drawrects)
+
+    # convert rects to polys
+    polygons = utils.rects2polys(rects, thetas, offsets, ratioWidth, ratioHeight)
+
+    for i, function in enumerate(functions):
+
+        start = time.time()
+        indicies = nms.polygons(polygons, confidences, nms_function=function, confidence_threshold=confidenceThreshold,
+                                nsm_threshold=nmsThreshold)
+        end = time.time()
+
+        indicies = np.array(indicies).reshape(-1)
+
+        if len(indicies) != 0:
+            drawpolys = np.array(polygons)[indicies]
+            drawrects_count = len(drawpolys)
+
+    return [drawpolys_count, drawrects_count]
 
 
 class Extractor:
@@ -26,7 +143,7 @@ class Extractor:
         print ('Find images in folder: {}'.format(len(images)))
 
         try:
-            with open('processed_files.csv', 'r') as f:
+            with open('csvs/processed_files.csv', 'r') as f:
                 processed_images = f.readlines()
         except:
             processed_images = []
@@ -48,7 +165,7 @@ class Extractor:
         for image_color in colors:
             colors_features.extend([image_color.rgb.r,image_color.rgb.g, image_color.rgb.b])
 
-        with open('color_features.csv', 'a') as f:
+        with open('csvs/color_features.csv', 'a') as f:
             feature_str = ', '.join(str(x) for x in colors_features)
             f.writelines(feature_str)
             f.writelines('\n')
@@ -58,7 +175,7 @@ class Extractor:
         gimg = color.colorconv.rgb2grey(im)
         contours = measure.find_contours(gimg, 0.8)
 
-        with open('shape_feature.csv', 'a') as f:
+        with open('csvs/shape_feature.csv', 'a') as f:
             f.writelines(str(len(contours)))
             f.writelines('\n')
 
@@ -70,14 +187,43 @@ class Extractor:
         preds = self.model.predict(x)
         preds = preds[0]
 
-        with open('object_features.csv', 'a') as f:
+        with open('csvs/object_features.csv', 'a') as f:
             feature_str = ','.join(str(x) for x in preds)
             f.writelines(feature_str)
             f.writelines('\n')
 
-        with open('processed_files.csv', 'a') as f:
+        # colorfulness
+        img = cv2.imread(path)
+        img = imutils.resize(img, width=250)
+        C = image_colorfulness(img)
+
+        with open('csvs/colorfulness_feature.csv', 'a') as f:
+            f.writelines(str(C))
+            f.writelines('\n')
+
+
+        # face detection
+        img = cv2.imread(path, cv2.IMREAD_COLOR)
+        imgtest1 = img.copy()
+        imgtest = cv2.cvtColor(imgtest1, cv2.COLOR_BGR2GRAY)
+        facecascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+        faces = facecascade.detectMultiScale(imgtest, scaleFactor=1.2, minNeighbors=5)
+
+        with open('csvs/face_feature.csv', 'a') as f:
+            f.writelines(str(len(faces)))
+            f.writelines('\n')
+
+        texts = text_detect(path, 'frozen_east_text_detection.pb', 0.5, 320, 320)
+        with open('csvs/text_feature.csv', 'a') as f:
+            f.writelines('{}, {}'.format(str(texts[0]), str(texts[1])))
+            f.writelines('\n')
+
+        with open('csvs/processed_files.csv', 'a') as f:
             f.writelines(str(img_path))
             f.writelines('\n')
+
 
         return
 
